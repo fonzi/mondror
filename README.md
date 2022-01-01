85afc9ce46f5e4f97d4c34c7467159edd6d30b83
Fonzi Vazquez <fonzi.vazquez@fmgsuite.com>
2022-01-01 00:57:23 -0600

    Added codes and artoworks

---
+++ README.md
@@ -0,0 +1,11 @@
+Peit Mondrian Generator == Mondror 
+
+A way to generate Mondrian abstract artwork by changing colors randomly on a given canvas. 
+Written in HTML/CSS/Powershell because I did not want to use javascript.
+
+Simple little work done when I was bored.
+
+To use 
+Run the powershell script 
+Open in the html file 
+Enjoy your masterpiece to sell as an NFT! 💰
---
+++ pw.ps1
@@ -0,0 +1,51 @@
+﻿$MyPath = 'C:\devel\mondror\mondror.css'
+$MyFile = Get-Content $MyPath
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*1*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]1[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*2*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]2[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*3*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]3[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*4*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]4[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*5*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]5[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+
+$num1 = Get-Random -Maximum 255
+$num2 = Get-Random -Maximum 255
+$num3 = Get-Random -Maximum 255
+$value = "rgb(" + $num1 + ", " + $num2 + ", " + $num3 + ");/*6*/" 
+$MyFile = $MyFile -replace "rgb[(]\d*, \d*, \d*[)];\/[*]6[*]\/", $value
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
